add43467c3a029ee3df0b891fc3a2340ea875235
Outrider99 <outrider.w@gmail.com>
2017-09-19 12:58:04 -0600

    Added eyes if statements

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -46,16 +46,67 @@ public class MonsterController
 		System.out.println("");
 		System.out.println("");
 		
+		george.setArmCount(5);
+		george.setBloop(true);
+		george.setEyeCount(5);
+		george.setTentacleAmount(5);
+		
 		interactWithMonster(george);
 	}
 	
 	private void interactWithMonster(MarshmellowMonster currentMonster)
 	{
 		Scanner myScanner = new Scanner(System.in);
-		System.out.println(currentMonster.getName() + " is back! He wants to know how many eyes you want to eat, please type in how many");
+		System.out.println(currentMonster.getName() + " is back! He grows stuff back. He wants to know how many eyes you want to eat, please type in how many");
 		int consumed = myScanner.nextInt();
 		currentMonster.setEyeCount(currentMonster.getEyeCount() - consumed);
 		System.out.println(currentMonster);
+		
+		
+		//arms
+		System.out.println("How many arms are you interested in eating? I have " + currentMonster.getArms());
+		int armEat = myScanner.nextInt();
+		
+		if(armEat == 0) 
+		{
+			System.out.println("Not hungry? So sad");
+		}
+		else if(armEat == 0)
+		{
+			System.out.println("Math must be hard for you");
+		}
+		else if(armEat - currentMonster.getArms() < 0)
+		{
+			System.out.println("you're not allowed to eat more then exists on me");
+		}
+		else
+		{
+			currentMonster.setArmCount(currentMonster.getArms() - armEat);
+			System.out.println("Ok, now I have this many arms " + currentMonster.getArms());
+		}
+		
+		
+		//eyes
+		System.out.println("How many eyes are you interested in eating? I have " + currentMonster.getEyeCount());
+		int eyeEat = myScanner.nextInt();
+		
+		if(eyeEat == 0) 
+		{
+			System.out.println("Not hungry? So sad");
+		}
+		else if(eyeEat == 0)
+		{
+			System.out.println("Math must be hard for you");
+		}
+		else if(eyeEat - currentMonster.getEyeCount() < 0)
+		{
+			System.out.println("you're not allowed to eat more then exists on me");
+		}
+		else
+		{
+			currentMonster.setEyeCount(currentMonster.getEyeCount() - eyeEat);
+			System.out.println("Ok, now I have this many arms " + currentMonster.getEyeCount());
+		}
 		
 		myScanner.close();
 	}
